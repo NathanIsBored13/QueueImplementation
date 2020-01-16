965a890a58daae3c37bb7b80ca5680ea8362e905
Nathan Smith <K036659@eltham-college.org.uk>
2020-01-16 12:53:04 +0000

    Add project files.

---
+++ QueImplementation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue(5);
+            string input;
+            do
+            {
+                Console.Write("> ");
+                input = Console.ReadLine();
+                if (input.ToLower() != "q")
+                {
+                    if (input.Split(" ")[0].ToLower() == "add")
+                    {
+                        queue.Add(Convert.ToInt32(input.Split(" ")[1]));
+                        Console.WriteLine("added value");
+                    }
+                    else if (input.Split(" ")[0].ToLower() == "remove")
+                    {
+                        Console.WriteLine(queue.Remove());
+                    }
+                    else Console.WriteLine("invalid command");
+                    Console.WriteLine("\n");
+                }
+            } while (input.ToLower() != "q");
+        }
+    }
+}
---
+++ QueImplementation/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueImplementation
+{
+    class Queue
+    {
+        private int front, back, count;
+        private int[] queue;
+        public Queue(int size)
+        {
+            queue = new int[size];
+            front = 0;
+            back = 0;
+            count = 0;
+        }
+        public bool isFull() => count == queue.Length;
+        public bool isEmpty() => count == 0;
+        public void Add(int value)
+        {
+            if (!isFull())
+            {
+                queue[front] = value;
+                front = (front + 1) % queue.Length;
+                count++;
+            }
+            else Console.WriteLine("Queue is already full");
+        }
+        public int Remove()
+        {
+            int ret;
+            if (!isEmpty())
+            {
+                ret = queue[back];
+                back = (back + 1) % queue.Length;
+                count--;
+            }
+            else
+            {
+                ret = -1;
+                Console.WriteLine("Queue is empty");
+            }
+            return ret;
+        }
+    }
+}
